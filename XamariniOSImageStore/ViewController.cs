27f9a4feb6fc8412e5d26399d743b9b20852e0c2
DelpinSusaiRaj <susairajs@outlook.com>
2017-09-19 19:49:04 +0530

    All files Added

---
+++ XamariniOSImageStore/ViewController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Foundation;
+using UIKit;
+
+namespace XamariniOSImageStore
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		string fileName;
+		string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+		int Count = 0;
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		partial void UIButton8_TouchUpInside(UIButton sender)
+		{
+			StoreImage("Xamarin.jpg");
+		}
+
+		public void StoreImage(string filename)
+		{
+			if (File.Exists(fileName))
+			{
+				Count++;
+				string filenames = "image" + Count.ToString() + ".jpg";
+				fileName = Path.Combine(folderPath, filenames);
+			}
+			else
+			{
+				fileName = Path.Combine(folderPath, "image.jpg");
+			}
+
+			var img = UIImage.FromFile(filename);
+
+			NSData image = img.AsJPEG();
+			NSError err = null;
+
+
+			image.Save(fileName, false, out err);
+
+			lblPath.Text = "File Path:" + fileName;
+			imgProfile.Image = UIImage.FromFile(fileName);
+
+		}  
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
---
+++ XamariniOSImageStore/ViewController.designer.cs
@@ -0,0 +1,41 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XamariniOSImageStore
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView imgProfile { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblPath { get; set; }
+
+        [Action ("UIButton8_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton8_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (imgProfile != null) {
+                imgProfile.Dispose ();
+                imgProfile = null;
+            }
+
+            if (lblPath != null) {
+                lblPath.Dispose ();
+                lblPath = null;
+            }
+        }
+    }
+}
